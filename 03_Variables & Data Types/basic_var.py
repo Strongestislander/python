ddabc16d14b90d828ef72f9af21ec9bbea90e5fa
Christian Habihirwe <Habihirwe6@gmail.com>
2022-10-18 22:24:37 -0500

    03_variables & Data Types added

---
+++ 03_Variables & Data Types/basic_var.py
@@ -0,0 +1,10 @@
+
+character_name = "John"
+character_age = 35
+
+print("There once was man named " + character_name + ",")
+print("he was " + str(character_age) + " years.")
+
+character_name = "Mike"
+print("He really liked the name, " + character_name + ".")
+print("And was not happy that he was only " + str(character_age) + ".")
