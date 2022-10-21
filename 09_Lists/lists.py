e07b3cd382858f81a8e36d16816b2c395768c43d
Christian Habihirwe <Habihirwe6@gmail.com>
2022-10-21 11:02:27 -0500

    09_Lists added

---
+++ 09_Lists/lists.py
@@ -0,0 +1,20 @@
+
+friends = ["tom", "mike", "marculles", "sean", "ashton"]
+
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[3])
+print(friends[4])
+print(friends[-1])
+print(friends[-2])
+print(friends[-3])
+print(friends[-4])
+print(friends[-5])
+print(friends[1:])
+print(friends[2:])
+print(friends[3:])
+print(friends[4])
+
+friends[1] = "john"
+print(friends[1])
---
+++ 09_Lists/readme.md
@@ -0,0 +1,3 @@
+# Lists Basic
+--------------------------------------
+This script going over how lists work
