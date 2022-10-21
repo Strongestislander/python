107b73470c811e2247665bb69ee76e35948bc6b8
Christian Habihirwe <Habihirwe6@gmail.com>
2022-10-21 11:32:08 -0500

    added 10_list functions with comments

---
+++ 10_List_functions/List_function.py
@@ -0,0 +1,52 @@
+
+lucky_numbers = [4,8,9,3,6,1,0]
+friends = ["tom", "mike", "marculles", "sean", "ashton"]
+
+print(lucky_numbers)
+print(friends)
+print("\n")
+
+#passes another list into your list
+friends.extend(lucky_numbers)
+print(friends)
+#adds another vaule into the list
+friends.append("creed")
+print(friends)
+
+#inserts a vaule into the list
+friends.insert(1, "kelly")
+print(friends)
+
+#removes a vaule from the list
+friends.remove("kelly")
+print(friends)
+
+print("\n")
+
+#removes everything from the list
+friends.clear
+friends = ["tom"]
+print(friends)
+
+friends = ["tom", "mike", "marculles", "sean", "ashton"]
+print(friends)
+#pop removes the last element from the list
+friends.pop( )
+print(friends)
+print("\n")
+
+#returns the index vaule of "mike"
+print(friends.index("mike"))
+
+friends = ["tom", "mike", "mike", "sean", "ashton"]
+print(friends)
+#returns how many times the vaule is in the list (mike is in the list twice.
+print(friends.count("mike"))
+#sorts the list in order
+friends.sort()
+print(friends)
+
+#copies a list
+friends2 = friends.copy()
+
+print(friends2)
