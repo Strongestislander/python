d42f1228d6242dc108a44731f84f4c89a0cda2d4
Christian Habihirwe <Habihirwe6@gmail.com>
2022-10-21 11:30:58 -0500

    updated 09_lists with comments

--- 09_Lists/lists.py
+++ 09_Lists/lists.py
@@ -1,20 +1,27 @@
+#creats a list with vaules
+friends = ["rich", "mike", "marculles", "sean", "ashton"]
 
-friends = ["tom", "mike", "marculles", "sean", "ashton"]
-
+#prints out the entire list
+print(friends)
+print("\n")
+#prints out a list based on index vaules
 print(friends[0])
 print(friends[1])
 print(friends[2])
 print(friends[3])
 print(friends[4])
+#print out list starting from last index vaule
 print(friends[-1])
 print(friends[-2])
 print(friends[-3])
 print(friends[-4])
 print(friends[-5])
+#print out entier list starting with stated index vaule
 print(friends[1:])
 print(friends[2:])
 print(friends[3:])
 print(friends[4])
 
+#changes the vaule in index[1] to set vaule and prints out only that index vaule
 friends[1] = "john"
 print(friends[1])
